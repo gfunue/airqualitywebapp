72148bc6eeb69fbeb61b8ea2981a4c9f12b20302
gfunue <fenue.genjang@gmail.com>
2023-03-02 15:26:38 -0600

    Comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TextField, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Card, CardContent, Typography } from "@mui/material";
 
+//Define a function component called App that uses useState to manage the state of the two input fields (city1 and city2).
 function App() {
   const [city1, setCity1] = useState("");
   const [city2, setCity2] = useState("");
@@ -8,6 +9,7 @@ function App() {
   const [data2, setData2] = useState(null);
   const [error, setError] = useState("");
 
+  //Use the fetchData function to fetch the latest air quality data for a city and update the corresponding value state. Throws and error if city not found or encounter error while fetching data.
   const fetchData = async (city, setDataFunc) => {
     try {
       const response = await fetch(`https://api.openaq.org/v2/latest?city=${(city)}`);
@@ -25,35 +27,24 @@ function App() {
       setDataFunc(null);
     }
   };
-
+//Handles submit when the "Compare" button is clicked.
   const handleSubmit = (event) => {
     event.preventDefault();
     fetchData(city1, setData1);
     fetchData(city2, setData2);
   };
 
+  //Handles submit when the "Compare" button is clicked. Render the component using attributes from material ui.These section display a form for user input and a table when the form is submitted with the enquired information from the AQ database or error text.
   return (
     <Card sx={{ maxWidth: 900, margin: 'auto', marginTop: 5 }}>
       <CardContent>
-    <Typography align='center' variant="h5" component="h1" marginBottom={4} gutterBottom>
-          Air Quality Assessment Tool
-        </Typography>
+    <Typography align='center' variant="h5" component="h1" marginBottom={4} gutterBottom>Air Quality Assessment Tool</Typography>
       <form onSubmit={handleSubmit}>
-      <div >
-      
-        <TextField label="City 1" value={city1} onChange={(e) => setCity1(e.target.value)} variant="outlined" margin="normal" fullWidth required />
-        <TextField
-          label="City 2"
-          value={city2}
-          onChange={(e) => setCity2(e.target.value)}
-          variant="outlined"
-          margin="normal"
-          fullWidth required/>
-          <Button variant="contained" color="primary" type="submit" sx={{ mt: 2, mb:6, display: "flex", margin:'auto'}}>
-          Compare
-        </Button>
-        </div>
-        
+            <div >
+                <TextField label="City 1" value={city1} onChange={(e) => setCity1(e.target.value)} variant="outlined" margin="normal" fullWidth required />
+                <TextField label="City 2" value={city2} onChange={(e) => setCity2(e.target.value)} variant="outlined" margin="normal" fullWidth required/>
+                <Button variant="contained" color="primary" type="submit" sx={{ mt: 2, mb:6, display: "flex", margin:'auto'}}>Compare</Button>
+            </div>
       </form>
       <div>
         {error && <p style={{ color: "red" }}>{error}</p>}
